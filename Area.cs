9ae31670cd029c42cf9a3abc91176fe1b2ee1212
Stonehenge <Stonehenge>
2015-11-26 01:05:43 +0300

    Вычисление площади прямоугольного треугольника по двум катетам

---
+++ Area.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AreaOfRightTriangleModule
+{
+    /*
+     . Тестовое задание: 
+• Напишите функцию, которая вычисляет площадь прямоугольного треугольника, принимая на вход длину каждой его стороны. Функция станет частью библиотеки поставляемой кампанией внешним клиентам.
+• Напишите юнит-тесты на нее. */
+    static class Area
+    {
+        /// <summary>
+        /// площадь прямоугольного треугольника по формуле S=(a*b)/2                
+        /// </summary>
+        /// <param name="cathetus1">катет 1</param>
+        /// <param name="cathetus2">катет 2</param>
+        /// <returns>площадь</returns>
+        public static double CalculteAreaOfRightTriangle(double cathetus1, double cathetus2)
+        {
+            const string messageError = "Длина катета {0} должна быть больше нуля";
+            if (cathetus1 <= 0)
+            {
+                throw new NotSupportedException(string.Format(messageError, "cathetus1"));
+            }
+            if (cathetus2 <= 0)
+            {
+                throw new NotSupportedException(string.Format(messageError, "cathetus2"));
+            }
+            return (cathetus1 * cathetus2) / 2;
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AreaOfRightTriangleModule
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void CalculteAreaOfRightTriangleTest1_1()
+        {
+            Area.CalculteAreaOfRightTriangle(0, 4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void CalculteAreaOfRightTriangleTest1_2()
+        {
+            Area.CalculteAreaOfRightTriangle(-10, 4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void CalculteAreaOfRightTriangleTest2_1()
+        {
+            Area.CalculteAreaOfRightTriangle(4, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void CalculteAreaOfRightTriangleTest2_2()
+        {
+            Area.CalculteAreaOfRightTriangle(10, -5);
+        }
+
+        [TestMethod]
+        public void CalculteAreaOfRightTriangleTest3()
+        {
+            var rezult = Area.CalculteAreaOfRightTriangle(2.5, 5);
+            Assert.AreEqual(rezult, 6.25);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void CalculteAreaOfRightTriangleTest4_1()
+        {
+            Area.CalculteAreaOfRightTriangle(0, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void CalculteAreaOfRightTriangleTest4_2()
+        {
+            Area.CalculteAreaOfRightTriangle(-1, -1);
+        }
+    }
+}
